5e674162941d42097f1a0ef5798020c32a15368b
AndreMour <andreluizmoura2004@outlook.com>
2025-10-21 21:55:01 -0300

    fix: validation text

--- src/screens/Home/validationSchema.ts
+++ src/screens/Home/validationSchema.ts
@@ -1,12 +1,5 @@
 import * as yup from "yup";
 
 export const validationSchema = yup.object({
-  word: yup
-    .string()
-    .required("Digite uma palavra antes de buscar.")
-    .max(50, "A palavra deve ter no máximo 50 caracteres.")
-    .matches(
-      /^[a-zA-Z\s-]+$/,
-      "Apenas letras, espaços e hífens são permitidos."
-    ),
+  word: yup.string().required("Whoops, can’t be empty…"),
 });
